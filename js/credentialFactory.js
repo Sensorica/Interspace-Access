317771f4174ecc21a5b5d7d2b43b963d5b3c433f
Jim Anastassiou <jim.anastassiou@gmail.com>
2016-08-03 13:36:14 -0400

    Create credentialFactory.js
    js for calling credential factory

---
+++ js/credentialFactory.js
@@ -0,0 +1,22 @@
+var erisC = require('eris-contracts');
+
+var erisdbURL = "http://localhost:1337/rpc";
+var accountData = require('/some/account/data.json');
+var contractManager = erisC.newContractManagerDev(erisdbURL, accountData);
+
+var abi, compiled;
+    try {
+        abi = fs.readJsonSync('contracts/build/Credentials.abi');
+        compiled = fs.readFileSync('contracts/build/Credentials.bin').toString();
+    } catch (error){
+        callback(error);
+        return;
+    }
+    var credentialFactory = contractManager.newContractFactory(abi);
+
+    credentialFactory.new({data: compiled}, function(error, contract){
+        if(error){
+            callback(error);
+            return;
+        }
+    })
